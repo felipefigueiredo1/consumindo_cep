9a8ce86cdb17357c2fbced8ded895aac6375af00
Felipe Figueiredo <felipefigueiredosal@gmail.com>
2022-03-28 10:29:37 -0300

    consumindo-api

--- viacep.php
+++ viacep.php
@@ -18,17 +18,20 @@ use \App\Controller\Controller;
 //Declara o numero do CEP setado no formulario
 $numero = isset($_POST['cep']) ? $_POST['cep'] : null;
 
-if (!preg_match('/^[0-9]{5,5}([- ]?[0-9]{3,3})?$/', $numero)) {
-    echo '<script>alert("Cep inválido")</script>';
-} else {
-    //Chama o controller para instanciar a classe que contem a API
-    $cep = new Controller;
-    //Se estiver tudo certo, retorna os dados do cep.
-    if ($cep->pegarCep($numero)) {
-        $dados = $cep->pegarCep($numero);
-    } else {
+if ($numero) {
+    if (!preg_match('/^[0-9]{5,5}([- ]?[0-9]{3,3})?$/', $numero)) {
         echo '<script>alert("Cep inválido")</script>';
+    } else {
+        //Chama o controller para instanciar a classe que contem a API
+        $cep = new Controller;
+        //Se estiver tudo certo, retorna os dados do cep.
+        if ($cep->pegarCep($numero)) {
+            $dados = $cep->pegarCep($numero);
+        } else {
+            echo '<script>alert("Cep inválido")</script>';
+        }
     }
 }
+
 
 
